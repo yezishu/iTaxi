ab0d3fbf9cf8785ce3052862a3f8f085d8c79fd1
Zishu.Ye <550987932@qq.com>
2017-08-09 17:43:48 +0800

    添加 CardView

---
+++ commonlibrary/src/main/java/yzs/commonlibrary/thridparty/autolayout/AutoCardView.java
@@ -0,0 +1,39 @@
+package yzs.commonlibrary.thridparty.autolayout;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.util.AttributeSet;
+
+import com.zhy.autolayout.AutoFrameLayout;
+import com.zhy.autolayout.utils.AutoLayoutHelper;
+
+/**
+ * Des：自适应匹配布局
+ * create by Zishu.Ye on 2017/8/9  10:46
+ */
+public class AutoCardView  extends CardView {
+    private final AutoLayoutHelper mHelper=new AutoLayoutHelper(this);
+    public AutoCardView(Context context) {
+        super(context);
+    }
+
+    public AutoCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public AutoFrameLayout.LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new AutoFrameLayout.LayoutParams(getContext(),attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if(!isInEditMode())
+            mHelper.adjustChildren();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
