ed0032343828d6436a8011fbb6b1cf98ede85cc6
yezishu <550987932@qq.com>
2017-08-10 23:32:04 +0800

    新建 poi search界面

---
+++ map/src/main/java/yzs/map/poisearch/POISearchActivity.kt
@@ -0,0 +1,38 @@
+package yzs.map.poisearch
+
+import android.os.Bundle
+
+import yzs.commonlibrary.base.CommonBaseRxMvpActivity
+import yzs.map.R
+
+/**
+ * Des：请输入标题
+ * create by zishu.ye
+ */
+class POISearchActivity : CommonBaseRxMvpActivity<POISearchPresenter>(), IPOISearchView {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poisearch)
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initListener() {
+
+    }
+
+    override fun initPresenter() {
+        POISearchPresenter(this)
+    }
+
+    override fun showFailInfo(errorInfo: String) {
+
+    }
+}
---
+++ map/src/main/java/yzs/map/poisearch/POISearchPresenter.java
@@ -0,0 +1,15 @@
+package yzs.map.poisearch;
+
+import yzs.commonlibrary.base.BaseRxPresenter;
+
+/**
+ * Des：请输入标题
+ * Created by zishu.ye
+ */
+public class POISearchPresenter extends BaseRxPresenter<IPOISearchView> {
+
+    public POISearchPresenter(IPOISearchView view) {
+        super(view);
+    }
+
+}
